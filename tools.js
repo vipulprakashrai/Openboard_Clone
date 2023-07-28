7f11905aa1816448eb78d03f8882808bf6486967
vipulprakashrai <103874813+vipulprakashrai@users.noreply.github.com>
2023-07-28 22:57:02 +0530

    sticky notes func

--- tools.js
+++ tools.js
@@ -2,6 +2,14 @@ let optionContainer = document.querySelector(".option-container");
 
 let toolContainer = document.querySelector(".tool-container");
 
+let pencilCont = document.querySelector(".pencil-tool-cont");
+let eraserCont = document.querySelector(".eraser-tool-cont");
+
+let sticky = document.querySelector(".notes");
+let body = document.body;
+console.log(body);
+
+
 let pencilIcon = document.querySelector(".pencil");
 let eraserIcon = document.querySelector(".eraser");
 
@@ -39,15 +47,40 @@ closeTools();
 
 pencilIcon.addEventListener("click", (event) => {
     isPencilContPresent = !isPencilContPresent
-    if(isPencilContPresent){
-        pencilIcon.style.display = "flex";
+    if(!isPencilContPresent){
+        pencilCont.style.display = "none";
     }
     else{
-        pencilIcon.style.display = "none";
+        pencilCont.style.display = "block";
     }
     
 })
 
 eraserIcon.addEventListener("click", (event) => {
-    eraserIcon.style.display = "flex";
+    isEraserContPresent = !isEraserContPresent
+    if(!isEraserContPresent){
+      eraserCont.style.display = "none";
+    }
+    else{
+    eraserCont.style.display = "block";
+    }
+})
+
+sticky.addEventListener("click", function(event){
+   console.log("click");
+    let stickyCont = document.createElement("div");
+    console.log(stickyCont);
+    stickyCont.setAttribute("class", "notes-container")
+    stickyCont.innerHTML = `
+         <div class="header-cont">
+             <div class="minimize"></div>
+             <div class="remove"></div>
+         </div>
+         <div class="note-cont">
+         <textarea></textarea>
+         </div>
+    `;
+   
+    body.appendChild(stickyCont);
+   
 })
